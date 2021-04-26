a72eea20f17ebd168cf31d6bd5798a2b98e4043e
Rafael Acer <rafaelsantiagocruz@outlook.com>
2021-04-25 23:45:44 -0500

    Creando el proyecto de verificar registros en SQL Server y C Sharp

---
+++ VerificarRegistros/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace VerificarRegistros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            SQL consultar = new SQL();
+            MessageBox.Show(consultar.consultarExistencia(textBox1.Text));
+        }
+    }
+}
---
+++ VerificarRegistros/SQL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using Microsoft.Data.SqlClient;
+namespace VerificarRegistros
+{
+    class SQL
+    {
+        public string consultarExistencia(String id)
+        {
+            using (SqlConnection Conexion = new SqlConnection(Properties.Settings.Default.CadenaDeConexion))
+            {
+                try
+                {
+                    Conexion.Open();
+                    Query.CommandText = "SELECT idUsuario FROM usuarios WHERE idUsuario='" + id + "'";
+                    Query.Connection = Conexion;
+                    consultar = Query.ExecuteReader();
+                    if (consultar.Read())
+                    {
+                        return "Ya existe: " + consultar.GetString(0);
+                    }
+                    else { return "No existe"; }
+                }
+                catch (SqlException)
+                {
+                    return "Error";
+                }
+            }
+
+        }
+        SqlDataReader consultar;
+        SqlCommand Query = new SqlCommand();
+    }
+}
